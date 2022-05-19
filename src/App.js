b1b09ab5e7b071113b06d80338f199c894264f55
Quoc Lam <nql2506200@gmail.com>
2022-05-19 20:42:36 +0700

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
 
+import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import InstagramLogin from 'react-instagram-login';
+import { Card, Image } from 'react-bootstrap';
+import './App.css';
+import DataFetching from './DataFetching';
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <DataFetching />
+//     </div>
+//   )
+// }
+
+// export default App
+// class App extends React.Component {
+
+// 	// Constructor
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.state = {
+// 			items: [],
+// 			DataisLoaded: false
+// 		};
+// 	}
+
+// 	// ComponentDidMount is used to
+// 	// execute the code
+// 	componentDidMount() {
+// 		fetch(
+// "https://jsonplaceholder.typicode.com/users")
+// 			.then((res) => res.json())
+// 			.then((json) => {
+// 				this.setState({
+// 					items: json,
+// 					DataisLoaded: true
+// 				});
+// 			})
+// 	}
+// 	render() {
+// 		const { DataisLoaded, items } = this.state;
+// 		if (!DataisLoaded) return <div>
+// 			<h1> Pleses wait some time.... </h1> </div> ;
+
+// 		return (
+// 		<div className = "App">
+// 			<h1> Fetch data from an api in react </h1> {
+// 				items.map((item) => (
+// 				<ol key = { item.id } >
+// 					User_Name: { item.username },
+// 					Full_Name: { item.name },
+// 					User_Email: { item.email }
+// 					</ol>
+// 				))
+// 			}
+// 		</div>
+// 	);
+// }
+// }
+
+// export default App;
+
+// // IGQVJWQWthN0dGcUhxVTRDaVdfNWpnVDN2aDBuQUMxVS1tMjhlSnFYSlhJOG5uZAVRFLUpfVjliaHp0RUtKR2ZAaVENLREZALUzNTRV92OWJWaWd4RkxyUmRkUFNHcGFldkItY1g1MmgycWxnd2hMXzh6MAZDZD
+// //IGQVJXTEJ0TDFWSnEzeWVRd3NzeEhTanJmMjJ1MW9VaUhYS2xqQlEtNmNkaGotQmw5VXJZAU080cmd4czFNTXJHVm42ZAjgxRWoxX2ZA5S0JNdTY0ZAmhySkhLQmMwOEMtdkxqWU93UkFHSERYZAThWZA09yXwZDZD
 function App() {
+
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState('');
+
+  const responseInstagram = (response) => {
+    console.log(response);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+    <InstagramLogin
+      clientId="438581111328693"
+      buttonText="Login with Instagram"
+      redirect_uri='https://localhost:3000//oauth/authorize'
+      onSuccess={responseInstagram}
+      onFailure={responseInstagram}
+      // scopes={['user_profile', 'user_media']}
+      // onLoginSuccess={(data) => console.log("Success")}
+      // onLoginFailure={(data) => console.log("Fail " +  JSON.stringify(data))}
+      // scopes={['basic']}
+      // onLoginSuccess={token => this.setState({ token })}
+      // onLoginFailure={data => this.setState({ failure: data })}
+    />
+    {/* <FontAwesome
+      name="instagram"
+    /> */}
     </div>
   );
 }
